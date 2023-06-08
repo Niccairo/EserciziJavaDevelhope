c74e81a36eb8e6a3d26d1f8360cef8de8fa53687
Niccolò <niccolocairone@gmail.com>
2023-06-08 09:56:30 +0200

    Esercizio2

---
+++ src/com/develhope/introduction/Esercizio2.java
@@ -0,0 +1,8 @@
+package com.develhope.introduction;
+
+public class Esercizio2 {
+	public static void main(String[] args) {
+		String myName = "Niccolò";
+		System.out.println("My name is " + myName);
+	}
+}
