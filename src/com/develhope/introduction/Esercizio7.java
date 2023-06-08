12fb5f60f5e1392d4a14f49082de69c5b2cb33f7
Niccolò <niccolocairone@gmail.com>
2023-06-08 18:09:35 +0200

    Esercizio 7

---
+++ src/com/develhope/introduction/Esercizio7.java
@@ -0,0 +1,12 @@
+package com.develhope.introduction;
+
+public class Esercizio7 {
+	public static void main(String[] args) {
+		String myName = "Niccolò";
+		if (myName.length() % 2 == 0) {
+			System.out.println("Il numero di lettere è pari");
+		} else {
+			System.out.println("Il numero di lettere è dispari");
+		}
+	}
+}
