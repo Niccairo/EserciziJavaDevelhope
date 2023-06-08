2989113089de3277fa7e81c6a1fdd6f1e2c1581e
Niccolò <niccolocairone@gmail.com>
2023-06-08 09:45:20 +0200

    aggiunto primo esercizio

---
+++ src/com/develhope/introduction/Start.java
@@ -0,0 +1,8 @@
+package com.develhope.introduction;
+
+public class Start {
+	public static void main(String[] args) {
+		String myBirthday = "17/07/1993";
+		System.out.println("hello develhope java student " + myBirthday);
+	}
+}
