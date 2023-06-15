0cbad4dda4314ed7ec6483509a988b2f5c600caf
Niccolò <niccolocairone@gmail.com>
2023-06-15 13:17:36 +0200

    Esercizio 12

---
+++ src/com/develhope/introduction/Esercizio12.java
@@ -0,0 +1,10 @@
+package com.develhope.introduction;
+
+public class Esercizio12 {
+	public static void main(String[] args) {
+		
+		for (int i = 5 ; i<26 ; i++) {
+			System.out.println(i);
+		}
+	}
+}
