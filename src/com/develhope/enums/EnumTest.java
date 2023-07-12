a25ecf3a38cf8f13e01c20c5c8f1e981aa8665cc
Niccolò <niccolocairone@gmail.com>
2023-07-12 19:04:43 +0200

    Esercizio Enum

---
+++ src/com/develhope/enums/EnumTest.java
@@ -0,0 +1,19 @@
+package com.develhope.enums;
+
+public class EnumTest {
+
+	public static void main(String[] args) {
+
+		Months myMonth = Months.JULY;
+		
+		for (Months month : Months.values()) {
+			
+			if(month.name().endsWith("Y")) {
+				System.out.println(month + " end with Y");
+			}else {
+				System.out.println(month + " doesn't end with Y");
+			}
+		}
+	}
+
+}
---
+++ src/com/develhope/enums/Months.java
@@ -0,0 +1,16 @@
+package com.develhope.enums;
+
+public enum Months {
+	JANUARY,
+	FEBRUARY, 
+	MARCH,
+	APRIL, 
+	MAY,
+	JUNE, 
+	JULY,
+	AUGUST,
+	SEPTEMBER, 
+	OCTOBER, 
+	NOVEMBER, 
+	DECEMBER;
+}
